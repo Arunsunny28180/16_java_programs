6e8d4e221ba146e438951bbd24e0f961c7e224a4
Arunsunny28180 <79589562+Arunsunny28180@users.noreply.github.com>
2021-05-26 11:44:35 +0530

    Add files via upload

---
+++ first15_programs/Hello.java
@@ -0,0 +1,13 @@
+ class Hello {
+  public static void main(String[] args) {
+  String name="Arun";
+    System.out.println("Hello ");
+    System.out.println(name);
+  }
+}
+
+
+
+output:
+     Hello 
+     Arun
---
+++ first15_programs/sum.java
@@ -0,0 +1,8 @@
+ class Sum {
+  public static void main(String[] args) {
+  int A=10;
+  int B=5;
+  System.out.println(A+B);
+    
+  }
+}
