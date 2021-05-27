50a7fca0cac51387ba4a20d0703b3e6622f19fd4
Arunsunny28180 <79589562+Arunsunny28180@users.noreply.github.com>
2021-05-27 17:51:55 +0530

    Add files via upload

---
+++ first15_programs/arrayForEach.java
@@ -0,0 +1,8 @@
+class arrayForEach {
+  public static void main(String[] args) {
+    String[] arr = {"santo","arun","lithin"};
+    for (String  names: arr){
+      System.out.println(names);
+       }
+    }
+}
---
+++ first15_programs/arrayLoop.java
@@ -0,0 +1,8 @@
+ class arrayLoop {
+  public static void main(String[] args) {
+ int arr[]={12,4,5,2};
+ for(int i=0;i<arr.length;i++){
+ System.out.println(arr[i]);
+       }
+    }
+}
